602fd00a4e04dc39873c490cd3aa68e0bcb686b1
Emilio Zuñiga <emizc2004@gmail.com>
2026-07-03 12:50:35 -0600

    ADD: Competencia 2 | Ejercicio 12. Streamlit

---
+++ Ejercicio12/.antlr/ExprParser.java
@@ -0,0 +1,191 @@
+// Generated from c:/Escuela/9no_semestre/lenguajes_y_automatasII_ej11-15_streamlit/Ejercicio12/Expr.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue"})
+public class ExprParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		PUBLIC=1, CLASS=2, STATIC=3, VOID=4, STRING=5, INT=6, SYSTEM=7, IF=8, 
+		NUM=9, IDF=10, TEXTO=11, PAR1=12, PAR2=13, LLAVE1=14, LLAVE2=15, CORCHETE1=16, 
+		CORCHETE2=17, IGUAL=18, MAS=19, MAYORIGUAL=20, MENORIGUAL=21, PUNTOCOMA=22, 
+		PUNTO=23, WS=24;
+	public static final int
+		RULE_root = 0, RULE_expr = 1;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"root", "expr"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'public'", "'class'", "'static'", "'void'", "'String'", "'int'", 
+			"'System'", "'if'", null, null, null, "'('", "')'", "'{'", "'}'", "'['", 
+			"']'", "'='", "'+'", "'>='", "'<='", "';'", "'.'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "PUBLIC", "CLASS", "STATIC", "VOID", "STRING", "INT", "SYSTEM", 
+			"IF", "NUM", "IDF", "TEXTO", "PAR1", "PAR2", "LLAVE1", "LLAVE2", "CORCHETE1", 
+			"CORCHETE2", "IGUAL", "MAS", "MAYORIGUAL", "MENORIGUAL", "PUNTOCOMA", 
+			"PUNTO", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Expr.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public ExprParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class RootContext extends ParserRuleContext {
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public TerminalNode EOF() { return getToken(ExprParser.EOF, 0); }
+		public RootContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_root; }
+	}
+
+	public final RootContext root() throws RecognitionException {
+		RootContext _localctx = new RootContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_root);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(4);
+			expr();
+			setState(5);
+			match(EOF);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class ExprContext extends ParserRuleContext {
+		public TerminalNode NUM() { return getToken(ExprParser.NUM, 0); }
+		public TerminalNode IDF() { return getToken(ExprParser.IDF, 0); }
+		public TerminalNode TEXTO() { return getToken(ExprParser.TEXTO, 0); }
+		public ExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_expr; }
+	}
+
+	public final ExprContext expr() throws RecognitionException {
+		ExprContext _localctx = new ExprContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_expr);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(7);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & 3584L) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\u0004\u0001\u0018\n\u0002\u0000\u0007\u0000\u0002\u0001\u0007\u0001\u0001"+
+		"\u0000\u0001\u0000\u0001\u0000\u0001\u0001\u0001\u0001\u0001\u0001\u0000"+
+		"\u0000\u0002\u0000\u0002\u0000\u0001\u0001\u0000\t\u000b\u0007\u0000\u0004"+
+		"\u0001\u0000\u0000\u0000\u0002\u0007\u0001\u0000\u0000\u0000\u0004\u0005"+
+		"\u0003\u0002\u0001\u0000\u0005\u0006\u0005\u0000\u0000\u0001\u0006\u0001"+
+		"\u0001\u0000\u0000\u0000\u0007\b\u0007\u0000\u0000\u0000\b\u0003\u0001"+
+		"\u0000\u0000\u0000\u0000";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
---
+++ Ejercicio12/Expr.g4
@@ -0,0 +1,33 @@
+grammar Expr;
+
+root: expr EOF ;    
+
+expr: NUM | IDF | TEXTO ;
+
+PUBLIC : 'public';
+CLASS : 'class';
+STATIC : 'static';
+VOID : 'void';
+STRING : 'String';
+INT : 'int';
+SYSTEM : 'System';
+IF : 'if';
+
+NUM: [0-9]+ ;
+IDF: [a-zA-Z]+[0-9a-zA-Z]*;
+TEXTO: '"' ~[\r\n"]* '"';
+
+PAR1: '(';
+PAR2: ')';
+LLAVE1: '{';
+LLAVE2: '}';
+CORCHETE1: '[';
+CORCHETE2: ']';
+IGUAL: '=';
+MAS: '+';
+MAYORIGUAL: '>=';
+MENORIGUAL: '<=';
+PUNTOCOMA: ';';
+PUNTO: '.';
+
+WS: [ \t\r\n]+ -> skip ;
